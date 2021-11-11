db58ec3abc2458bfbfd71baa0c944f9715a25e37
Muhammad-Ibrahim-Anjum <69748200+Muhammad-Ibrahim-Anjum@users.noreply.github.com>
2021-11-11 14:26:04 +0500

    Updated a little

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -17,24 +17,47 @@ export default function Textform(props) {
         setText(event.target.value);
     }
     const [text, setText] = useState("Enter Text Here");
+
+    const [btnText, setBtnText] = useState("Enable Dark Mode")
+
+    const [myStyle, setMyStyle] = useState({
+        color : 'black',
+        backgroundColor : 'white'
+    })
+    const toggleStyle = () => {
+        if(myStyle.color === 'white'){
+            setMyStyle({
+                color : 'black',
+                backgroundColor : 'white' 
+            })
+            setBtnText("Enable Light Mode");
+        }else{
+            setMyStyle({
+                color : 'white',
+                backgroundColor : 'black'
+            })
+            setBtnText("Enable Dark Mode");
+        }
+    }
     return ( 
         <>
-            <div>
+        <div className="container" style={myStyle}>
                 <h1>{props.heading}</h1>
-                <div className="mb-3">
+                <div className="mb-3" style={myStyle}>
                     <textarea className="form-control" value={text} onChange={getUserText} id="exampleFormControlTextarea1" rows="3"></textarea>
                 </div>
                 <button className = "btn btn-primary mx-1" onClick={actionUpOnClick}>Convert to UpperCase</button>
                 <button className = "btn btn-primary mx-1" onClick={actionLowOnClick}>Convert to LowerCase</button>
                 <button className = "btn btn-primary mx-1" onClick={actionClearOnClick}>Clear Text Area</button>
             </div>
-            <div className="container my3">
+            <div style={myStyle} className="container my3">
                 <h2>Text Summary</h2>
                 <p>{text.split(" ").length} Words and {text.length} Characters</p>
                 <p>{0.018 * text.split(" ").length} Minutes to read</p>
                 <h2>Preview</h2>
                 <p>{text}</p>
             </div>
+            <button onClick={toggleStyle} className="btn btn-outline-success" type="submit">{btnText}</button>   
         </>
     )
 }
